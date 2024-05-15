af0190d83029fe963c0b315ef80914372f539940
Gianluca Fiorentino <122386633+Gianluca-F@users.noreply.github.com>
2024-05-15 23:43:00 +0200

    Continued tests

--- exercise2/zmytest/exercise2a/fulltest.cpp
+++ exercise2/zmytest/exercise2a/fulltest.cpp
@@ -35,8 +35,7 @@
 
 /* ************************************************************************** */
 
-#define NUM 20
-#define MAX 50      
+#define NUM_BT 20 // Number of elements in the binary tree
 
 /* ************************************************************************** */
 
@@ -87,7 +86,12 @@ void myTestBTint(uint & testnum, uint & testerr) {
     // BinaryTreeVec empty
     cout << "\n~~~ BinaryTreeVec empty ~~~\n" << endl;
     lasd::BinaryTreeVec<int> btv_empty;
+    lasd::BinaryTreeLnk<int> btv2_empty;
+    lasd::BinaryTreeVec<int> btv3_empty(btv_empty);
     Root<int>(loctestnum, loctesterr, btv_empty, false, 10);
+    EqualBT<int>(loctestnum, loctesterr, btv_empty, btv2_empty);
+    EqualBT<int>(loctestnum, loctesterr, btv_empty, btv3_empty);
+    EqualBT<int>(loctestnum, loctesterr, btv2_empty, btv3_empty);
 
     // BinaryTreeVec with one element
     cout << "\n~~~ BinaryTreeVec with one element (created using a vector) ~~~\n" << endl;
@@ -121,9 +125,9 @@ void myTestBTint(uint & testnum, uint & testerr) {
 
     // BinaryTreeVec with many elements
     cout << "\n~~~ BinaryTreeVec with many elements (created using a vector) ~~~\n" << endl;
-    lasd::Vector<int> v2(NUM);
+    lasd::Vector<int> v2(NUM_BT);
     uint i;
-    for (i = 0; i < NUM; i++) {
+    for (i = 0; i < NUM_BT; i++) {
       v2[i] = i;
     }
     lasd::BinaryTreeVec<int> btv_many(v2);
@@ -163,7 +167,9 @@ void myTestBTint(uint & testnum, uint & testerr) {
     // BinaryTreeLnk empty
     cout << "\n~~~ BinaryTreeLnk empty ~~~\n" << endl;
     lasd::BinaryTreeLnk<int> btl_empty;
+    lasd::BinaryTreeLnk<int> btl2_empty;
     Root<int>(loctestnum, loctesterr, btl_empty, false, 0);
+    EqualBT<int>(loctestnum, loctesterr, btl_empty, btl2_empty);
 
     // BinaryTreeLnk with one element
     cout << "\n~~~ BinaryTreeLnk with one element (created using a vector) ~~~\n" << endl;
@@ -233,6 +239,16 @@ void myTestBTint(uint & testnum, uint & testerr) {
     NonEqualBT<int>(loctestnum, loctesterr, btv_many, btl_many_copy);
     NonEqualBT<int>(loctestnum, loctesterr, btv_many_copy, btl_many);
 
+    btv_many.Clear();
+    EqualBT<int>(loctestnum, loctesterr, btv_many, btl_empty);
+    NonEqualBT<int>(loctestnum, loctesterr, btv_many, btl_many);
+
+    btl_many.Clear();
+    EqualBT<int>(loctestnum, loctesterr, btl_empty, btv_empty);
+    NonEqualBT<int>(loctestnum, loctesterr, btl_many, btv_many_copy);
+
+    EqualBT<int>(loctestnum, loctesterr, btv_many, btl_empty);
+
   } catch (exception& exc) {
     cout << "Caught exception: " << exc.what() << endl;
     loctestnum++;
@@ -246,11 +262,199 @@ void myTestBTint(uint & testnum, uint & testerr) {
 
 /* ************************************************************************** */
 
+template <typename Data>
+void EqualIt(uint & testnum, uint & testerr, const lasd::BTBreadthIterator<Data> & it1, const lasd::BTBreadthIterator<Data> & it2) {
+  bool tst;
+  testnum++;
+  cout << " " << testnum << " (" << testerr << ") The two iterators are " << ((tst = (it1 == it2)) ? "" : "not ") << "equal: ";
+  cout << (tst ? "Correct" : "Error") << "!" << std::endl;
+  testerr += (1 - (uint) tst);
+}
+
+template <typename Data>
+void NonEqualIt(uint & testnum, uint & testerr, const lasd::BTBreadthIterator<Data> & it1, const lasd::BTBreadthIterator<Data> & it2) {
+  bool tst;
+  testnum++;
+  cout << " " << testnum << " (" << testerr << ") The two iterators are " << ((tst = (it1 != it2)) ? "not " : "") << "equal: ";
+  cout << (tst ? "Correct" : "Error") << "!" << std::endl;
+  testerr += (1 - (uint) tst);
+}
+
+template <typename Data>
+void EqualIt(uint & testnum, uint & testerr, const lasd::BTPreOrderIterator<Data> & it1, const lasd::BTPreOrderIterator<Data> & it2) {
+  bool tst;
+  testnum++;
+  cout << " " << testnum << " (" << testerr << ") The two iterators are " << ((tst = (it1 == it2)) ? "" : "not ") << "equal: ";
+  cout << (tst ? "Correct" : "Error") << "!" << std::endl;
+  testerr += (1 - (uint) tst);
+}
+
+template <typename Data>
+void NonEqualIt(uint & testnum, uint & testerr, const lasd::BTPreOrderIterator<Data> & it1, const lasd::BTPreOrderIterator<Data> & it2) {
+  bool tst;
+  testnum++;
+  cout << " " << testnum << " (" << testerr << ") The two iterators are " << ((tst = (it1 != it2)) ? "not " : "") << "equal: ";
+  cout << (tst ? "Correct" : "Error") << "!" << std::endl;
+  testerr += (1 - (uint) tst);
+}
+
+template <typename Data>
+void EqualIt(uint & testnum, uint & testerr, const lasd::BTPostOrderIterator<Data> & it1, const lasd::BTPostOrderIterator<Data> & it2) {
+  bool tst;
+  testnum++;
+  cout << " " << testnum << " (" << testerr << ") The two iterators are " << ((tst = (it1 == it2)) ? "" : "not ") << "equal: ";
+  cout << (tst ? "Correct" : "Error") << "!" << std::endl;
+  testerr += (1 - (uint) tst);
+}
+
+template <typename Data>
+void NonEqualIt(uint & testnum, uint & testerr, const lasd::BTPostOrderIterator<Data> & it1, const lasd::BTPostOrderIterator<Data> & it2) {
+  bool tst;
+  testnum++;
+  cout << " " << testnum << " (" << testerr << ") The two iterators are " << ((tst = (it1 != it2)) ? "not " : "") << "equal: ";
+  cout << (tst ? "Correct" : "Error") << "!" << std::endl;
+  testerr += (1 - (uint) tst);
+}
+
+template <typename Data>
+void EqualIt(uint & testnum, uint & testerr, const lasd::BTInOrderIterator<Data> & it1, const lasd::BTInOrderIterator<Data> & it2) {
+  bool tst;
+  testnum++;
+  cout << " " << testnum << " (" << testerr << ") The two iterators are " << ((tst = (it1 == it2)) ? "" : "not ") << "equal: ";
+  cout << (tst ? "Correct" : "Error") << "!" << std::endl;
+  testerr += (1 - (uint) tst);
+}
+
+template <typename Data>
+void NonEqualIt(uint & testnum, uint & testerr, const lasd::BTInOrderIterator<Data> & it1, const lasd::BTInOrderIterator<Data> & it2) {
+  bool tst;
+  testnum++;
+  cout << " " << testnum << " (" << testerr << ") The two iterators are " << ((tst = (it1 != it2)) ? "not " : "") << "equal: ";
+  cout << (tst ? "Correct" : "Error") << "!" << std::endl;
+  testerr += (1 - (uint) tst);
+}
+
+/* ************************************************************************** */
+
 void myTestBTdouble(uint & testnum, uint & testerr) {
   uint loctestnum = 0, loctesterr = 0;
-  cout << endl << "Begin of BinaryTree<double> Test:" << endl;
+  cout << endl << "Begin of BinaryTree<double> Test (using iterators):" << endl;
 
   try {
+    // BinaryTree empty
+    cout << "\n~~~ BinaryTree empty ~~~\n" << endl;
+    lasd::BinaryTreeVec<double> btv_empty;
+    lasd::BinaryTreeLnk<double> btl_empty;
+
+    lasd::BTBreadthMutableIterator<double> mut_it_bt_empty(btv_empty);
+    Terminated<double>(loctestnum, loctesterr, mut_it_bt_empty, true);
+    GetItrValue<double>(loctestnum, loctesterr, mut_it_bt_empty, false, 5.0);
+    SetItrValue<double>(loctestnum, loctesterr, mut_it_bt_empty, false, 10.2);
+
+    lasd::BTBreadthIterator<double> it_bt_empty(mut_it_bt_empty);
+    Terminated<double>(loctestnum, loctesterr, it_bt_empty, true);
+    GetItrValue<double>(loctestnum, loctesterr, it_bt_empty, false, 5.0);
+
+    lasd::BTPreOrderMutableIterator<double> mut_it2_bt_empty(btv_empty);
+    Terminated<double>(loctestnum, loctesterr, mut_it2_bt_empty, true);
+    GetItrValue<double>(loctestnum, loctesterr, mut_it2_bt_empty, false, 5.0);
+    SetItrValue<double>(loctestnum, loctesterr, mut_it2_bt_empty, false, 10.2);
+
+    lasd::BTPreOrderIterator<double> it2_bt_empty(move(mut_it2_bt_empty));
+    Terminated<double>(loctestnum, loctesterr, it2_bt_empty, true);
+    GetItrValue<double>(loctestnum, loctesterr, it2_bt_empty, false, 5.0);
+
+    lasd::BTPostOrderMutableIterator<double> mut_it3_bt_empty(btl_empty);
+    Terminated<double>(loctestnum, loctesterr, mut_it3_bt_empty, true);
+    GetItrValue<double>(loctestnum, loctesterr, mut_it3_bt_empty, false, 5.0);
+    SetItrValue<double>(loctestnum, loctesterr, mut_it3_bt_empty, false, 10.2);
+
+    lasd::BTPostOrderIterator<double> it3_bt_empty(mut_it3_bt_empty);
+    Terminated<double>(loctestnum, loctesterr, it3_bt_empty, true);
+    GetItrValue<double>(loctestnum, loctesterr, it3_bt_empty, false, 5.0);
+
+    lasd::BTInOrderMutableIterator<double> mut_it4_bt_empty(btl_empty);
+    Terminated<double>(loctestnum, loctesterr, mut_it4_bt_empty, true);
+    GetItrValue<double>(loctestnum, loctesterr, mut_it4_bt_empty, false, 5.0);
+    SetItrValue<double>(loctestnum, loctesterr, mut_it4_bt_empty, false, 10.2);
+
+    lasd::BTInOrderIterator<double> it4_bt_empty(move(mut_it4_bt_empty));
+    Terminated<double>(loctestnum, loctesterr, it4_bt_empty, true);
+    GetItrValue<double>(loctestnum, loctesterr, it4_bt_empty, false, 5.0);
+
+    /* ************************************************************************************************* */
+
+    // BinaryTree with one element
+    cout << "\n~~~ BinaryTree with one element ~~~\n" << endl;
+    lasd::Vector<double> v1(1);  v1[0] = -5.2;
+    lasd::BinaryTreeVec<double> btv_one(v1);
+    lasd::List<double> l1;  l1.InsertAtBack(3.18);
+    lasd::BinaryTreeLnk<double> btl_one(l1);
+
+    lasd::BTBreadthMutableIterator<double> mut_it_bt_one(btv_one);
+    Terminated<double>(loctestnum, loctesterr, mut_it_bt_one, false);
+    GetItrValue<double>(loctestnum, loctesterr, mut_it_bt_one, true, -5.2);
+    SetItrValue<double>(loctestnum, loctesterr, mut_it_bt_one, true, 10.2);
+
+    lasd::BTBreadthIterator<double> it_bt_one(btv_one);
+    Terminated<double>(loctestnum, loctesterr, it_bt_one, false);
+    GetItrValue<double>(loctestnum, loctesterr, it_bt_one, true, 10.2);
+
+    EqualIt<double>(loctestnum, loctesterr, mut_it_bt_one, it_bt_one);
+    Terminated<double>(loctestnum, loctesterr, it_bt_one, false);
+    ++it_bt_one;
+    Terminated<double>(loctestnum, loctesterr, it_bt_one, true);
+    NonEqualIt<double>(loctestnum, loctesterr, mut_it_bt_one, it_bt_one);
+    it_bt_one.Reset();
+    EqualIt<double>(loctestnum, loctesterr, mut_it_bt_one, it_bt_one);
+
+    lasd::BTPreOrderMutableIterator<double> mut_it2_bt_one(btl_one);
+    Terminated<double>(loctestnum, loctesterr, mut_it2_bt_one, false);
+    GetItrValue<double>(loctestnum, loctesterr, mut_it2_bt_one, true, 3.18);
+    SetItrValue<double>(loctestnum, loctesterr, mut_it2_bt_one, true, 10.2);
+
+    lasd::BTPreOrderIterator<double> it2_bt_one(move(mut_it2_bt_one));
+    Terminated<double>(loctestnum, loctesterr, it2_bt_one, false);
+    GetItrValue<double>(loctestnum, loctesterr, it2_bt_one, true, 10.2);
+
+    NonEqualIt<double>(loctestnum, loctesterr, mut_it2_bt_one, it2_bt_one);
+
+    lasd::BTPostOrderMutableIterator<double> mut_it3_bt_one(btv_one);
+    Terminated<double>(loctestnum, loctesterr, mut_it3_bt_one, false);
+    GetItrValue<double>(loctestnum, loctesterr, mut_it3_bt_one, true, 10.2);
+    SetItrValue<double>(loctestnum, loctesterr, mut_it3_bt_one, true, -5.2);
+
+    lasd::BTPostOrderIterator<double> it3_bt_one(btv_one);
+    Terminated<double>(loctestnum, loctesterr, it3_bt_one, false);
+    GetItrValue<double>(loctestnum, loctesterr, it3_bt_one, true, -5.2);
+
+    EqualIt<double>(loctestnum, loctesterr, mut_it3_bt_one, it3_bt_one);
+    ++it3_bt_one;
+    Terminated<double>(loctestnum, loctesterr, it3_bt_one, true);
+    ++mut_it3_bt_one;
+    Terminated<double>(loctestnum, loctesterr, mut_it3_bt_one, true);
+    EqualIt<double>(loctestnum, loctesterr, mut_it3_bt_one, it3_bt_one);
+    it3_bt_one.Reset();
+    NonEqualIt<double>(loctestnum, loctesterr, mut_it3_bt_one, it3_bt_one);
+    mut_it3_bt_one.Reset();
+    EqualIt<double>(loctestnum, loctesterr, mut_it3_bt_one, it3_bt_one);
+
+    lasd::BTInOrderMutableIterator<double> mut_it4_bt_one(btl_one);
+    Terminated<double>(loctestnum, loctesterr, mut_it4_bt_one, false);
+    GetItrValue<double>(loctestnum, loctesterr, mut_it4_bt_one, true, 10.2);
+    SetItrValue<double>(loctestnum, loctesterr, mut_it4_bt_one, true, 3.18);
+
+    lasd::BTInOrderIterator<double> it4_bt_one(move(mut_it4_bt_one));
+    Terminated<double>(loctestnum, loctesterr, it4_bt_one, false);
+    GetItrValue<double>(loctestnum, loctesterr, it4_bt_one, true, 3.18);
+
+    NonEqualIt<double>(loctestnum, loctesterr, mut_it4_bt_one, it4_bt_one);
+
+    /* ************************************************************************************************* */
+
+    // BinaryTree with many elements
+    cout << "\n~~~ BinaryTree with many elements ~~~\n" << endl;
+
 
   } catch (exception& exc) {
     cout << "Caught exception: " << exc.what() << endl;
