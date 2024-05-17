6e808bc923cc605ea9aabbf581de614a2a9753fb
Gianluca Fiorentino <122386633+Gianluca-F@users.noreply.github.com>
2024-05-18 00:08:52 +0200

    Continued test

--- exercise2/zmytest/exercise2b/fulltest.cpp
+++ exercise2/zmytest/exercise2b/fulltest.cpp
@@ -92,9 +92,9 @@ void myTestBSTint(uint & testnum, uint & testerr) {
     EqualBT<int>(loctestnum, loctesterr, bst, btv_empty);
     EqualBST<int>(loctestnum, loctesterr, bst, bst);
 
-    bst.Clear();
+    btl_empty.Clear(); btv_empty.Clear(); bst.Clear();
 
-    /* ************************************************************* */
+    /* ************************************************************************************************* */
 
     // trees with one element
     cout << "\n~~~ BST with one element tests ~~~\n" << endl;
@@ -143,10 +143,12 @@ void myTestBSTint(uint & testnum, uint & testerr) {
     Empty(loctestnum, loctesterr, bst, true);
     Size(loctestnum, loctesterr, bst, true, 0);
 
-    /* ************************************************************* */
+    v.Clear(); btv.Clear(); btl.Clear();
 
-    // trees with many elements
-    cout << "\n~~~ BST with many elements tests ~~~\n" << endl;
+    /* ************************************************************************************************* */
+
+    // trees with many (random) elements
+    cout << "\n~~~ BST with many (random) elements tests ~~~\n" << endl;
     int min = MAX_BST, max = -1*MAX_BST;
 
     lasd::Vector<int> v2(NUM_BST);
@@ -160,76 +162,169 @@ void myTestBSTint(uint & testnum, uint & testerr) {
     lasd::List<int> l(v2);
     lasd::BinaryTreeVec<int> btv2(v2);
     lasd::BinaryTreeLnk<int> btl2(l);
-    InsertAllC(loctestnum, loctesterr, bst, true, v2);
+    InsertAllC<int>(loctestnum, loctesterr, bst, true, v2);
 
     Empty(loctestnum, loctesterr, bst, false);
     Size(loctestnum, loctesterr, bst, true, NUM_BST);
 
     for(uint i = 0; i < NUM_BST; i++) {
-      Exists(loctestnum, loctesterr, bst, true, v2[i]);
+      Exists<int>(loctestnum, loctesterr, bst, true, v2[i]);
     }
 
-    Root(loctestnum, loctesterr, bst, true, v2[0]);
-    Min(loctestnum, loctesterr, bst, true, min);
-    Max(loctestnum, loctesterr, bst, true, max);
-    EqualBST(loctestnum, loctesterr, bst, bst);
+    Root<int>(loctestnum, loctesterr, bst, true, v2[0]);
+    Min<int>(loctestnum, loctesterr, bst, true, min);
+    Max<int>(loctestnum, loctesterr, bst, true, max);
+    EqualBST<int>(loctestnum, loctesterr, bst, bst);
 
-    TraverseInOrder(loctestnum, loctesterr, bst, true, &TraversePrint<int>);
+    TraverseInOrder<int>(loctestnum, loctesterr, bst, true, &TraversePrint<int>);
 
     lasd::BST<int> bst2(bst);
-    EqualBST(loctestnum, loctesterr, bst, bst2);
+    EqualBST<int>(loctestnum, loctesterr, bst, bst2);
 
-    MinNRemove(loctestnum, loctesterr, bst2, true, min);
-    MaxNRemove(loctestnum, loctesterr, bst2, true, max);
+    MinNRemove<int>(loctestnum, loctesterr, bst2, true, min);
+    MaxNRemove<int>(loctestnum, loctesterr, bst2, true, max);
     Size(loctestnum, loctesterr, bst2, true, NUM_BST-2);
-    NonEqualBST(loctestnum, loctesterr, bst, bst2);
+    NonEqualBST<int>(loctestnum, loctesterr, bst, bst2);
 
-    RemoveMin(loctestnum, loctesterr, bst2, true);
+    RemoveMin<int>(loctestnum, loctesterr, bst2, true);
     Size(loctestnum, loctesterr, bst2, true, NUM_BST-3);
 
-    RemoveMax(loctestnum, loctesterr, bst2, true);
+    RemoveMax<int>(loctestnum, loctesterr, bst2, true);
     Size(loctestnum, loctesterr, bst2, true, NUM_BST-4);
 
-    TraverseInOrder(loctestnum, loctesterr, bst2, true, &TraversePrint<int>);
+    TraverseInOrder<int>(loctestnum, loctesterr, bst2, true, &TraversePrint<int>);
 
     lasd::BST<int> bst3(move(bst2));
     bst2.Clear();
     Size(loctestnum, loctesterr, bst3, true, NUM_BST-4);
 
-    RemoveAll(loctestnum, loctesterr, bst3, false, l);
+    RemoveAll<int>(loctestnum, loctesterr, bst3, false, l);
     Size(loctestnum, loctesterr, bst3, true, 0);
 
-    RemoveSome(loctestnum, loctesterr, bst3, false, v2);
+    RemoveSome<int>(loctestnum, loctesterr, bst3, false, v2);
     Size(loctestnum, loctesterr, bst3, true, 0);
 
-    InsertAllM(loctestnum, loctesterr, bst3, true, move(l));
+    InsertAllM<int>(loctestnum, loctesterr, bst3, true, move(l));
     Size(loctestnum, loctesterr, bst3, true, NUM_BST);
-    EqualBST(loctestnum, loctesterr, bst, bst3);
+    EqualBST<int>(loctestnum, loctesterr, bst, bst3);
 
     bst3.Clear();
 
     v2[1] = MAX_BST+1;
-    InsertAllM(loctestnum, loctesterr, bst3, true, move(v2));
-    Min(loctestnum, loctesterr, bst3, true, min);
-    Max(loctestnum, loctesterr, bst3, true, MAX_BST+1);
+    InsertAllM<int>(loctestnum, loctesterr, bst3, true, move(v2));
+    Min<int>(loctestnum, loctesterr, bst3, true, min);
+    Max<int>(loctestnum, loctesterr, bst3, true, MAX_BST+1);
     Size(loctestnum, loctesterr, bst3, true, NUM_BST);
 
-    TraverseInOrder(loctestnum, loctesterr, bst3, true, &TraversePrint<int>);
-    NonEqualBST(loctestnum, loctesterr, bst, bst3);
+    TraverseInOrder<int>(loctestnum, loctesterr, bst3, true, &TraversePrint<int>);
+    NonEqualBST<int>(loctestnum, loctesterr, bst, bst3);
 
     bst2 = move(bst3);
     Size(loctestnum, loctesterr, bst2, true, NUM_BST);
-    Exists(loctestnum, loctesterr, bst2, true, MAX_BST+1);
-    NonEqualBST(loctestnum, loctesterr, bst, bst2);
-    TraverseInOrder(loctestnum, loctesterr, bst2, true, &TraversePrint<int>);
+    Exists<int>(loctestnum, loctesterr, bst2, true, MAX_BST+1);
+    NonEqualBST<int>(loctestnum, loctesterr, bst, bst2);
+    TraverseInOrder<int>(loctestnum, loctesterr, bst2, true, &TraversePrint<int>);
 
     bst2 = bst;
     Size(loctestnum, loctesterr, bst2, true, NUM_BST);
-    EqualBST(loctestnum, loctesterr, bst, bst2);
-    TraverseInOrder(loctestnum, loctesterr, bst2, true, &TraversePrint<int>);
+    EqualBST<int>(loctestnum, loctesterr, bst, bst2);
+    TraverseInOrder<int>(loctestnum, loctesterr, bst2, true, &TraversePrint<int>);
 
-    /* ************************************************************* */
+    v2.Clear(); l.Clear(); btv2.Clear(); btl2.Clear(); bst.Clear(); bst2.Clear(); bst3.Clear();
 
+    /* ************************************************************************************************* */
+
+    // trees with many (decided) elements
+
+    cout << "\n~~~ BST with many (decided) elements tests ~~~\n" << endl;
+
+    lasd::Vector<int> v4(10);
+                                                  v4[0] = 10;
+    //                                          /              \  .
+                          v4[1] = 5;                                       v4[2] = 15; 
+    //                  /           \                                    /           \  .
+            v4[3] = 3;                v4[4] = 7;            v4[5] = 12;                v4[6] = 17;
+    //       /    \                   /
+    v4[7] = 1;  v4[8] = 4;    v4[9] = 6;
+
+    lasd::List<int> l4;
+    for(uint i = 0; i < 10; i++) { l4.InsertAtFront(v4[i]); }
+
+    lasd::BinaryTreeVec<int> btv_v4(v4);
+    lasd::BinaryTreeLnk<int> btl_v4(v4);
+    lasd::BinaryTreeVec<int> btv_l4(l4);
+    lasd::BinaryTreeLnk<int> btl_l4(l4);
+    lasd::BST<int> bst_v4(v4);
+    lasd::BST<int> bst_l4(l4);
+
+    TraverseInOrder<int>(loctestnum, loctesterr, btv_v4, true, &TraversePrint<int>);
+    TraverseInOrder<int>(loctestnum, loctesterr, btl_v4, true, &TraversePrint<int>);
+    TraverseInOrder<int>(loctestnum, loctesterr, btv_l4, true, &TraversePrint<int>);
+    TraverseInOrder<int>(loctestnum, loctesterr, btl_l4, true, &TraversePrint<int>);
+
+    Traverse<int>(loctestnum, loctesterr, bst_v4, true, &TraversePrint<int>);
+
+    // Comparison tests
+    cout << "\n~~~ BST Comparison tests ~~~\n" << endl;
+
+    EqualBT<int>(loctestnum, loctesterr, bst_v4, btv_v4);
+    EqualBT<int>(loctestnum, loctesterr, bst_v4, btl_v4);
+
+    NonEqualBT<int>(loctestnum, loctesterr, btv_v4, btv_l4);
+    NonEqualBT<int>(loctestnum, loctesterr, btl_v4, btl_l4);
+    NonEqualBT<int>(loctestnum, loctesterr, bst_v4, btv_l4);
+    NonEqualBT<int>(loctestnum, loctesterr, bst_l4, btl_l4);
+
+    EqualBST<int>(loctestnum, loctesterr, bst_v4, bst_l4);
+
+    bst_l4.Clear();
+
+    /* ************************************************************************ */
+
+
+    //                                               v4[0] = 10;
+    // //                                          /              \  .
+    //                       v4[1] = 5;                                       v4[2] = 15; 
+    // //                  /           \                                    /           \  .
+    //         v4[3] = 3;                v4[4] = 7;            v4[5] = 12;                v4[6] = 17;
+    // //       /    \                   /
+    // v4[7] = 1;  v4[8] = 4;    v4[9] = 6;
+
+    // Pred and Succ tests
+    cout << "\n~~~ BST Predecessor and Successor tests ~~~\n" << endl;
+
+    // Case 1: value exists in the tree
+    Predecessor<int>(loctestnum, loctesterr, bst_v4, true, 10, 7);
+    Predecessor<int>(loctestnum, loctesterr, bst_v4, true, 5, 4);
+    Predecessor<int>(loctestnum, loctesterr, bst_v4, true, 15, 12);
+    Predecessor<int>(loctestnum, loctesterr, bst_v4, true, 17, 15);
+    Predecessor<int>(loctestnum, loctesterr, bst_v4, true, 12, 10);
+    Predecessor<int>(loctestnum, loctesterr, bst_v4, false, 1, 0);
+
+    Successor<int>(loctestnum, loctesterr, bst_v4, true, 10, 12);
+    Successor<int>(loctestnum, loctesterr, bst_v4, true, 5, 6);
+    Successor<int>(loctestnum, loctesterr, bst_v4, true, 1, 3);
+    Successor<int>(loctestnum, loctesterr, bst_v4, true, 7, 10);
+    Successor<int>(loctestnum, loctesterr, bst_v4, true, 12, 15);
+    Successor<int>(loctestnum, loctesterr, bst_v4, false, 17, 19);
+
+    // Case 2: value does not exist in the tree
+    Predecessor<int>(loctestnum, loctesterr, bst_v4, true, 11, 10);
+    Predecessor<int>(loctestnum, loctesterr, bst_v4, true, 8, 7);
+    Predecessor<int>(loctestnum, loctesterr, bst_v4, true, 13, 12);
+    Predecessor<int>(loctestnum, loctesterr, bst_v4, true, 19, 17);
+    Predecessor<int>(loctestnum, loctesterr, bst_v4, false, 0, -1);
+
+    Successor<int>(loctestnum, loctesterr, bst_v4, true, 8, 10);
+    Successor<int>(loctestnum, loctesterr, bst_v4, true, 11, 12);
+    Successor<int>(loctestnum, loctesterr, bst_v4, true, 13, 15);
+    Successor<int>(loctestnum, loctesterr, bst_v4, true, 0, 1);
+    Successor<int>(loctestnum, loctesterr, bst_v4, false, 19, 20);
+
+    // with remove  
+
+
+    /* ************************************************************************************************* */
 
   } catch (exception & exc) {
     cout << "Caught exception: " << exc.what() << endl;
